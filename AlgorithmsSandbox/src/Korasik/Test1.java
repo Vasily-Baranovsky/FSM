7b1b7ba36dc04bff8aa6752eb12af570b1c4f619
Vasily-Baranovsky <Vasily.Baranovsky@yandex.ru>
2021-12-03 08:26:40 +0100

    Implementing of Trie filling and  simple printing

---
+++ AlgorithmsSandbox/src/Korasik/Test1.java
@@ -0,0 +1,14 @@
+package Korasik;
+
+public class Test1 {
+    public static void main(String[] args) {
+        TrieNode tn = new TrieNode('.');
+
+        tn.addWord("test");
+        tn.addWord("ttest");
+        tn.addWord("tttest");
+
+
+
+    }
+}
--- AlgorithmsSandbox/src/Korasik/TrieNode.java
+++ AlgorithmsSandbox/src/Korasik/TrieNode.java
@@ -1,26 +1,28 @@
 package Korasik;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
 public class TrieNode {
-    protected List<TrieNode> children= Collections.emptyList();
-    protected String charValue;
+    protected List<TrieNode> children= new ArrayList<TrieNode>();
+    protected Character charValue;
     protected boolean endOfWord=false;
 
-    public TrieNode(String value) {
+    public TrieNode(Character value) {
         this.charValue = value;
     }
 
-    public void addChild(String charValue) {
+    public void addChild(Character charValue) {
         if (children.stream().noneMatch(it -> it.charValue.equals(charValue))) {
             children.add(new TrieNode(charValue));
         }
     }
 
-    public TrieNode findOrCreateChild(String charValue) {
+    public TrieNode findOrCreateChild(Character charValue) {
 
+        // хеш мэпы для лохов!
         return children.stream().filter(it -> it.charValue.equals(charValue))
                 .findFirst()
                 .orElseGet(() -> {
@@ -31,8 +33,29 @@ public class TrieNode {
     }
 
     public void addWord(String word) {
-        for (int i=0; i<word.length(); ++i) {
 
+        TrieNode tn = this;
+        for (int i=0; i<word.length(); ++i) {
+            tn = tn.findOrCreateChild(word.charAt(i));
         }
+
+        tn.endOfWord = true;
+    }
+
+    @Override
+    public String toString() {
+        String res = charValue.toString() + " [";
+        for (int i=0; i<children.size(); ++i) {
+            res += children.get(i).toString();
+        }
+        res +="]";
+
+        return res;
+    }
+
+    public void print() {
+        TrieNode tn = this;
+
+        System.out.println(tn.toString());
     }
 }
