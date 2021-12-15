6807a21d44556cfa2512854b502bbf7dd90261a4
anezbudi <aleksei.nezbudii@t-systems.com>
2021-12-15 10:29:50 +0300

    the breut search implememted without test

--- AlgorithmsSandbox/src/BrutStringSearch/BrutStringSearch.java
+++ AlgorithmsSandbox/src/BrutStringSearch/BrutStringSearch.java
@@ -1,5 +1,6 @@
 package BrutStringSearch;
 
+import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -15,9 +16,35 @@ public class BrutStringSearch implements SubstringSearcher{
 
 
     @Override
-    public HashMap<String, List<Integer>> searchString(String s) {
-        
+    public HashMap<String, List<Integer>> searchString(String initialString) {
+        // набор фактов на какой позиции какой символ есть - мы можем получить символ на любой позиции строки s, аналогично для строк в словаре
+        // создание гиптез - делаем ленивый алгоритм
 
-        return null;
+        HashMap<String, List<Integer>> resultSubstringMap = new HashMap<>();
+
+        for (int i = 0; i < initialString.length(); i++) {
+            for (int j = 0; j < dictionary.size(); j++) {
+                boolean isSubstring = true;
+                String searchableString = dictionary.get(j);
+                for (int k = 0; k < searchableString.length(); k ++) {
+                    if (initialString.charAt(i + k) != searchableString.charAt(k)) {
+                        isSubstring = false;
+                        break;
+                    }
+                }
+                if (isSubstring) {
+                    List<Integer> indexes = resultSubstringMap.computeIfAbsent(searchableString, k -> new ArrayList<>());
+                    indexes.add(i);
+                }
+            }
+        }
+
+        // проверка гипотез
+        // сохранение живых гипотез
+        // возвращение набора результирующих фактов (выжившие гипотезы, которые стали фактами) в виде HashMap
+
+
+
+        return resultSubstringMap;
     }
 }
