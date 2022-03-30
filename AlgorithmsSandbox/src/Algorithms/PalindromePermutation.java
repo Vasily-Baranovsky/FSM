0d9c6257f10bfdfb342122328caf4ae8f21ca96b
Vasily-Baranovsky <Vasily.Baranovsky@yandex.ru>
2022-03-30 08:15:37 +0200

    задача PolindromePermutation. Рабочий вариант с генерацией всех перестановок (брутфорс алгоритм)

--- AlgorithmsSandbox/src/Algorithms/PalindromePermutation.java
+++ AlgorithmsSandbox/src/Algorithms/PalindromePermutation.java
@@ -1,50 +1,65 @@
 package Algorithms;
 
 import java.util.ArrayList;
-import java.util.stream.Collectors;
 
 public class PalindromePermutation {
     public static void main(String[] args) {
         //String s1 = "Tact Coa";
-        String s = "xyz";
+        String s = "xyzxyz";
 
         ArrayList<String> arrResult = getPermutations(s);
 
         for (int i=0; i<arrResult.size(); ++i) {
-            System.out.println(arrResult.get(i));
+            System.out.println(arrResult.get(i) + " - " + isPalindrome(arrResult.get(i)));
         }
+
+        System.out.println("Final result - " + isPalindromeExists(arrResult));
     }
 
     public static ArrayList<String> getPermutations(String str) {
         ArrayList<String> arrRes = new ArrayList<>();
         char[] chArr = str.toCharArray();
-        char[] chSubArray = str.substring(1).toCharArray();
 
-        getPermutations(0, chSubArray, arrRes);
+        getPermutations(0, chArr, arrRes);
 
         return arrRes;
     }
 
     public static void getPermutations(int startIndex, char[] str, ArrayList<String> arrRes) {
-        if (arrRes == null) {
-            arrRes = new ArrayList<>();
-        }
 
-        if (str.length < 2) {
+        if (startIndex >= str.length-1) {
+            arrRes.add(new String(str));
             return;
         }
 
-
-        for (int i = startIndex+1; i < str.length; i++) {
+        for (int i = startIndex; i < str.length; i++) {
             char[] chCopy=str.clone();
-            char chTmp = chCopy[0];
-            chCopy[0]=chCopy[i];
+            char chTmp = chCopy[startIndex];
+            chCopy[startIndex]=chCopy[i];
             chCopy[i]=chTmp;
 
-            arrRes.add(new String(chCopy));
-
-            getPermutations(startIndex+1, str, arrRes);
+            getPermutations(startIndex+1, chCopy, arrRes);
         }
+    }
+
+    public static boolean isPalindrome(String str) {
+        for (int i=0; i<str.length()/2; ++i) {
+            if (str.charAt(i) != str.charAt(str.length()-i-1)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static boolean isPalindromeExists( ArrayList<String> arrayOfPermutations) {
+        for (int i = 0; i < arrayOfPermutations.size(); i++) {
+            if (isPalindrome(arrayOfPermutations.get(i))) {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
 
